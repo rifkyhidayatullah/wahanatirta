a90fe8dbd9d377ebab76f266d7e724dd24e6a6a7
rifkyhidayatullah <50349431+rifkyhidayatullah@users.noreply.github.com>
2026-05-06 17:15:50 +0700

    logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,9 +1,9 @@
 <?php
-session_start(); // Memulai sesi
-$_SESSION = []; // Mengosongkan data sesi
-session_unset(); // Menghapus variabel sesi
-session_destroy(); // Menghancurkan sesi
+session_start(); 
+$_SESSION = []; 
+session_unset(); 
+session_destroy(); 
 
-header("Location: login.php"); // Kembali ke login
+header("Location: login.php"); 
 exit;
 ?>
